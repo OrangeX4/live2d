11ff9c7bd4e8ffdb4846856b916c4fc5f443a51c
OrangeX4 <34951714+OrangeX4@users.noreply.github.com>
2020-01-03 07:21:54 +0800

    Add files via upload

---
+++ content-script.js
@@ -0,0 +1,23 @@
+﻿function injectCustomJs(jsPath)
+{
+    jsPath = jsPath || 'js/inject.js';
+    var temp = document.createElement('script');
+    temp.setAttribute('type', 'text/javascript');
+    // 获得的地址类似：chrome-extension://ihcokhadfjfchaeagdoclpnjdiokfakg/js/inject.js
+    temp.src = chrome.extension.getURL(jsPath);
+    temp.onload = function()
+    {
+        // 放在页面不好看，执行完后移除掉
+        //this.parentNode.removeChild(this);
+    };
+    document.head.appendChild(temp);
+}
+
+(function() {
+	console.log('这是 simple-chrome-plugin-demo 的content-script！');
+    injectCustomJs("https://cdn.jsdelivr.net/npm/jquery/dist/jquery.min.js");
+    injectCustomJs("https://cdn.jsdelivr.net/npm/font-awesome/css/font-awesome.min.css");
+    injectCustomJs("https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget/autoload.js");
+    injectCustomJs("autoload.js");
+    injectCustomJs("jquery.min.js");
+})();
